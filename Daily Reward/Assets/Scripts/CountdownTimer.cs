5bfee181e27019b3ba8066e6b64f99b36ccf670b
Ayush Agarwal <1999ayush@gmail.com>
2019-10-14 09:13:49 +0530

    This is a daily reward system providing rewards at variable times.

---
+++ Daily Reward/Assets/Scripts/CountdownTimer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountdownTimer : MonoBehaviour
+{
+    TimeSpan countdownTimer;
+
+    [SerializeField] Text countdownText;
+    public void GetCountdown(TimeSpan x) //Gets the remaining time from DailyReward script
+    {
+        countdownTimer = x;
+        InvokeRepeating("StartCountDown", 1, 1);
+    }
+
+    private void StartCountDown()
+    {
+        countdownTimer = countdownTimer.Subtract(new TimeSpan(0, 0, 1));
+        if (countdownTimer.TotalSeconds > 0)
+        {
+            countdownText.text = countdownTimer.ToString();
+        }
+    }
+}
---
+++ Daily Reward/Assets/Scripts/SceneManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneManager : MonoBehaviour
+{
+    public void ClaimReward() 
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("DailyReward");
+    }
+
+    public void GoBack()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Start Menu");
+    }
+   public void OnQuitClick()
+    {
+        Application.Quit();
+    }
+}
